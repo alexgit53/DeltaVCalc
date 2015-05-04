d9d524f7cd0e95fff296415bc39b734e184edc7c
alexgit53 <alexgit53@gmail.com>
2015-05-04 16:27:35 +0100

    Refactoring, and add burn time output

--- DeltaVCalc.py
+++ DeltaVCalc.py
@@ -5,48 +5,16 @@ from math import log, trunc
 __author__ = 'Alex'
 
 DELIMITER = "----------"
-TWR_DEC_PLACES = 2
+TWR_DEC_PLACES = 1
 SURFACE_GRAVITIES = [("Kerbin", 9.81), ("Mun", 1.63), ("Minmus", 0.491), ("Duna", 2.94)]
 
 
 def main():
     while True:
-        print("Enter stage details")
-
-        # Get the stage values
-        m_init = get_typed_input("Initial mass: ", float)
-        m_end = get_typed_input("End mass: ", float)
-        # If we have no masses, we can't do any calculations, so start again
-        if None in (m_init, m_end):
-            continue
-        isp = get_typed_input("Engine ISP: ", float)
-        thrust = get_typed_input("Total thrust: ", float)
-
-        # Calculate our values if we have the data, or set them to a string if not
         try:
-            delta_v = abs(trunc(calculate_delta_v(m_init, m_end, isp)))
-        except TypeError:
-            delta_v = "?"
-
-        try:
-            acc_init = abs(trunc(thrust / m_init))
-            acc_end = abs(trunc(thrust / m_end))
-            TWRs = ["TWR on {0}: {1} - {2}".format(planet,
-                                                   str(round(acc_init / gravity, TWR_DEC_PLACES)),  # Min TWR
-                                                   str(round(acc_end / gravity, TWR_DEC_PLACES))  # Max TWR
-                                                   )
-                    for (planet, gravity) in SURFACE_GRAVITIES]
-        except TypeError:
-            acc_end = "?"
-            acc_init = "?"
-            TWRs = ["TWRs: ?"]
-
-        # Display our calculated values, and offer to repeat
-        print(DELIMITER)
-        print("Delta v: " + str(delta_v) + " m/s")
-        print("Acceleration range: {0} - {1} m/s^2".format(str(min(acc_end, acc_init)), str(max(acc_end, acc_init))))
-        print("\n".join(TWRs))
-        print(DELIMITER)
+            get_and_process_stage()
+        except ValueError:
+            print("Unphysical values")
         input("Enter to begin again, or Ctrl+c to close...")
 
 
@@ -66,6 +34,56 @@ def get_typed_input(prompt, type_function):
             continue
         else:
             return returned_value
+
+
+def get_and_process_stage():
+    print("Enter stage details")
+    # Get the stage values
+    m_init = get_typed_input("Initial mass: ", float)
+    m_end = get_typed_input("End mass: ", float)
+    # If we have no masses, we can't do any calculations, so start again
+    if None in (m_init, m_end):
+        print("Masses are required")
+        return
+    isp = get_typed_input("Engine ISP: ", float)
+    thrust = get_typed_input("Total thrust: ", float)
+
+    # Calculate our values if we have the data, or set them to a string if not
+    try:
+        delta_v = abs(trunc(calculate_delta_v(m_init, m_end, isp)))
+    except TypeError:
+        delta_v = "?"
+
+    try:
+        acc_init = abs(trunc(thrust / m_init))
+        acc_end = abs(trunc(thrust / m_end))
+        TWRs = ["TWR on {0}: {1} - {2}".format(planet,
+                                               str(round(acc_init / gravity, TWR_DEC_PLACES)),  # Min TWR
+                                               str(round(acc_end / gravity, TWR_DEC_PLACES))  # Max TWR
+                                               )
+                for (planet, gravity) in SURFACE_GRAVITIES]
+    except TypeError:
+        acc_end = "?"
+        acc_init = "?"
+        TWRs = ["TWRs: ?"]
+
+    try:
+        burn_time_m, burn_time_s = divmod(2 * delta_v / (acc_end + acc_init), 60)
+        burn_time_s = trunc(burn_time_s)
+        burn_time_m = trunc(burn_time_m)
+    except ValueError:
+        burn_time_m = "?"
+        burn_time_s = "?"
+
+    # Display our calculated values, and offer to repeat
+    print(DELIMITER)
+    print("Delta v: {0} m/s".format(str(delta_v)))
+    print("Acceleration range: {0} - {1} m/s^2".format(str(min(acc_end, acc_init)), str(max(acc_end, acc_init))))
+    print("Stage time: {0}m {1}s".format(str(burn_time_m), str(burn_time_s)))
+    print("\n".join(TWRs))
+    print(DELIMITER)
+
+    return
 
 
 if __name__ == "__main__":
